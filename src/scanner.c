e46be4002e996375efbe601006eed0a800a47a57
C.y <yangruihan@vip.qq.com>
2018-12-11 14:23:04 +0800

    add scanner

--- src/scanner.c
+++ src/scanner.c
@@ -17,3 +17,33 @@ void initScanner(const char* source) {
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token errorToken(const char* message) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = message;
+    token.length = (int)strlen(message);
+    token.line = scanner.line;
+
+    return token;
+}
+
+Token scanToken() {
+    scanner.start = scanner.current;
+    if (isAtEnd()) return makeToken(TOKEN_EOF);
+    return errorToken("Unexpected character.");
+}
