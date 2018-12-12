7455cc1fe5c706f73ab8dda3d73f623f0d8b0979
C.y <yangruihan@vip.qq.com>
2018-12-12 17:29:01 +0800

    update compiler

--- src/compiler.c
+++ src/compiler.c
@@ -1,28 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
+typedef struct {
+    Token current;
+    Token previous;
+    bool  hadError;
+    bool  panicMode;
+} Parser;
+
+Parser parser;
+
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode)
+        return;
+    parser.panicMode = true;
+    
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void error(const char* message) {
+    errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
 
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-
-        printf("%2d '%.*s'\n",
-               token.type,
-               token.length,
-               token.start);
-
-        if (token.type == TOKEN_EOF) break;
-    }
+    compilingChunk = chunk;
+    parser.hadError = false;
+    parser.panicMode = false;
+    
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
 
