fa7eddce99d1782165ac2489d16ce9466379f141
C.y <yangruihan@vip.qq.com>
2018-12-13 11:38:29 +0800

    compiling expression

--- src/compiler.c
+++ src/compiler.c
@@ -5,12 +5,38 @@
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
 typedef struct {
     Token current;
     Token previous;
     bool  hadError;
     bool  panicMode;
 } Parser;
+
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM,       // + -
+    PREC_FACTOR,     // * /
+    PREC_UNARY,      // ! - +
+    PREC_CALL,       // . () []
+    PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
 
 Parser parser;
 
@@ -79,8 +105,146 @@ static void emitReturn() {
     emitByte(OP_RETURN);
 }
 
+static uint8_t makeConstant(Value value) {
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
 static void endCompiler() {
     emitReturn();
+
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError) {
+        disassembleChunk(currentChunk(), "code");
+    }
+#endif
+}
+
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+    TokenType operatorType = parser.previous.type;
+
+    ParseRule* rule = getRule(operatorType);
+    parsePrecedence((Precedence)(rule->precedence + 1));
+
+    switch (operatorType) {
+    case TOKEN_PLUS:
+        emitByte(OP_ADD);
+        break;
+    case TOKEN_MINUS:
+        emitByte(OP_SUBTRACT);
+        break;
+    case TOKEN_STAR:
+        emitByte(OP_MULTIPLY);
+        break;
+    case TOKEN_SLASH:
+        emitByte(OP_DIVIDE);
+        break;
+    default:
+        return;
+    }
+}
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary() {
+    TokenType operatorType = parser.previous.type;
+
+    // Compile the operand.
+    parsePrecedence(PREC_UNARY);
+
+    switch (operatorType) {
+    case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+    default: return;
+    }
+}
+
+ParseRule rules[] = {                                              
+    { grouping, NULL,    PREC_CALL },       // TOKEN_LEFT_PAREN      
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_PAREN     
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_LEFT_BRACE
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RIGHT_BRACE     
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_COMMA           
+    { NULL,     NULL,    PREC_CALL },       // TOKEN_DOT             
+    { unary,    binary,  PREC_TERM },       // TOKEN_MINUS           
+    { NULL,     binary,  PREC_TERM },       // TOKEN_PLUS            
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_SEMICOLON       
+    { NULL,     binary,  PREC_FACTOR },     // TOKEN_SLASH           
+    { NULL,     binary,  PREC_FACTOR },     // TOKEN_STAR            
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_BANG            
+    { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_BANG_EQUAL      
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_EQUAL           
+    { NULL,     NULL,    PREC_EQUALITY },   // TOKEN_EQUAL_EQUAL     
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_GREATER         
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_GREATER_EQUAL   
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_LESS            
+    { NULL,     NULL,    PREC_COMPARISON }, // TOKEN_LESS_EQUAL      
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_STRING          
+    { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
+    { NULL,     NULL,    PREC_AND },        // TOKEN_AND             
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE            
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FALSE           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FUN             
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_FOR             
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_IF              
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_NIL             
+    { NULL,     NULL,    PREC_OR },         // TOKEN_OR              
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN          
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_THIS            
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_TRUE            
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_VAR             
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_ERROR           
+    { NULL,     NULL,    PREC_NONE },       // TOKEN_EOF             
+};   
+
+static void parsePrecedence(Precedence precedence) {
+    advance();
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+    if (prefixRule == NULL) {
+        error("Expect expression.");
+        return;
+    }
+
+    prefixRule();
+
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        infixRule();
+    }
+}
+
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
+}
+
+void expression() {
+    parsePrecedence(PREC_ASSIGNMENT);
 }
 
 bool compile(const char* source, Chunk* chunk) {
