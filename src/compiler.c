21427ae080c68447b73acbabadb5ca03e40e71d7
C.y <yangruihan@vip.qq.com>
2019-05-19 17:41:14 +0800

    using locals

--- src/compiler.c
+++ src/compiler.c
@@ -180,6 +180,7 @@ static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 static uint8_t identifierConstant(Token* name);
+static int resolveLocal(Compiler* compiler, Token* name);
 
 static void binary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
@@ -227,13 +228,22 @@ static void string(bool canAssign) {
 }
 
 static void namedVariable(Token name, bool canAssign) {
-    int arg = identifierConstant(&name);
+    uint8_t getOp, setOp;
+    int arg = resolveLocal(current, &name);
+    if (arg != -1) {
+        getOp = OP_GET_LOCAL;
+        setOp = OP_SET_LOCAL;
+    } else {
+        arg = identifierConstant(&name);
+        getOp = OP_GET_GLOBAL;
+        setOp = OP_SET_GLOBAL;
+    }
 
     if (canAssign && match(TOKEN_EQUAL)) {
         expression();
-        emitBytes(OP_SET_GLOBAL, (uint8_t)arg);
+        emitBytes(setOp, (uint8_t)arg);
     } else {
-        emitBytes(OP_GET_GLOBAL, (uint8_t)arg);
+        emitBytes(getOp, (uint8_t)arg);
     }
 }
 
@@ -327,6 +337,16 @@ static uint8_t identifierConstant(Token* name) {
 static bool identifiersEqual(Token* a, Token* b) {
     if (a->length != b->length) return false;
     return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static int resolveLocal(Compiler* compiler, Token* name) {
+    for (int i = compiler->localCount - 1; i >= 0; i--) {
+        Local* local = &compiler->locals[i];
+        if (identifiersEqual(name, &local->name)) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 static void addLocal(Token name) {
--- src/vm.c
+++ src/vm.c
@@ -116,6 +116,18 @@ static InterpretResult run() {
         case OP_FALSE:    push(BOOL_VAL(false)); break;
         case OP_POP:      pop(); break;
 
+        case OP_GET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            push(vm.stack[slot]);
+            break;
+        }
+
+        case OP_SET_LOCAL: {
+            uint8_t slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
+
         case OP_GET_GLOBAL: {
             ObjString* name = READ_STRING();
             Value value;
