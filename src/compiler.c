23b0ed7895602a68ab3fa66ff596268a14d042b4
C.y <yangruihan@vip.qq.com>
2019-05-26 14:47:28 +0800

    Logical Operators

--- src/compiler.c
+++ src/compiler.c
@@ -200,6 +200,8 @@ static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 static uint8_t identifierConstant(Token* name);
 static int resolveLocal(Compiler* compiler, Token* name);
+static void and_(bool canAssign);
+static void or_(bool canAssign);
 
 static void binary(bool canAssign) {
     TokenType operatorType = parser.previous.type;
@@ -239,6 +241,17 @@ static void grouping(bool canAssign) {
 static void number(bool canAssign) {
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
+}
+
+static void or_(bool canAssign) {
+    int elseJump = emitJump(OP_JUMP_IF_FALSE);
+    int endJump = emitJump(OP_JUMP);
+
+    patchJump(elseJump);
+    emitByte(OP_POP);
+
+    parsePrecedence(PREC_OR);
+    patchJump(endJump);
 }
 
 static void string(bool canAssign) {
@@ -306,7 +319,7 @@ ParseRule rules[] = {
     { variable, NULL,    PREC_NONE },       // TOKEN_IDENTIFIER      
     { string,   NULL,    PREC_NONE },       // TOKEN_STRING          
     { number,   NULL,    PREC_NONE },       // TOKEN_NUMBER          
-    { NULL,     NULL,    PREC_AND },        // TOKEN_AND             
+    { NULL,     and_,    PREC_AND },        // TOKEN_AND             
     { NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
     { NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE            
     { literal,  NULL,    PREC_NONE },       // TOKEN_FALSE           
@@ -314,7 +327,7 @@ ParseRule rules[] = {
     { NULL,     NULL,    PREC_NONE },       // TOKEN_FOR             
     { NULL,     NULL,    PREC_NONE },       // TOKEN_IF              
     { literal,  NULL,    PREC_NONE },       // TOKEN_NIL             
-    { NULL,     NULL,    PREC_OR },         // TOKEN_OR              
+    { NULL,     or_,     PREC_OR },         // TOKEN_OR              
     { NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT           
     { NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN          
     { NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER           
@@ -419,6 +432,15 @@ static void defineVariable(uint8_t global) {
     }
     
     emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
+static void and_(bool canAssign) {
+    int endJump = emitJump(OP_JUMP_IF_FALSE);
+
+    emitByte(OP_POP);
+    parsePrecedence(PREC_AND);
+
+    patchJump(endJump);
 }
 
 static ParseRule* getRule(TokenType type) {
